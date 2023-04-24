b5281e85281c8c85210a76defe2d2efbbb8b1a4b
y4wee <quentin.jambert@gmail.com>
2023-04-24 15:01:55 +0200

    installation sqlite3 and typeorm

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const typeorm = require("typeorm");
+
+const app = express();
+
+const dataSource = new typeorm.DataSource({
+    type: "sqlite",
+    database: "./wildersdb.sqlite",
+    synchronize: true,
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+const start = async () => {
+    await dataSource.initialize();
+    app.listen(3000, () => console.log("Server started on 3000"));
+};
+//Start Server
+
+start();
