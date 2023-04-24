5b98183a8f29da03e59b068aa083a12defbf9d7c
y4wee <quentin.jambert@gmail.com>
2023-04-24 17:36:02 +0200

    update request done

--- src/controller/wilder.js
+++ src/controller/wilder.js
@@ -4,7 +4,8 @@ const Wilder = require("../entity/Wilder");
 module.exports = {
     create: async (req, res) => {
         try {
-            await dataSource.getRepository(Wilder).save(req.body);
+            const repository = dataSource.getRepository(Wilder);
+            await repository.save(req.body);
             res.send("Created wilder");
         } catch (error) {
             res.send("Error while creating wilder : " + error);
@@ -13,11 +14,34 @@ module.exports = {
 
     getAll: async (req, res) => {
         try {
-            const wilders = await dataSource.getRepository(Wilder).find();
-            res.send(wilders)
+            const repository = dataSource.getRepository(Wilder);
+            const wilders = await repository.find();
+            res.send(wilders);
         } catch (error) {
             console.error(error);
         }
     },
 
+    updateOne: async (req, res) => {
+        try {
+            const repository = dataSource.getRepository(Wilder);
+            const user = await repository.findOneBy({
+                id: req.body.id,
+            });
+            user.name = req.body.name;
+            await repository.save(user);
+            res.send("wilder updated");
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    deleteOne: async (req, res) => {
+        try {
+            const wilders = await dataSource.getRepository(Wilder).find();
+            res.send(wilders);
+        } catch (error) {
+            console.error(error);
+        }
+    },
 };
--- src/index.js
+++ src/index.js
@@ -9,13 +9,12 @@ app.use(express.json());
 
 app.post("/api/wilder", wilderController.create);
 app.get("/api/wilder", wilderController.getAll);
-app.delete("/api/wilder", wilderController.getAll);
-app.put("/api/wilder", wilderController.getAll);
+app.delete("/api/wilder", wilderController.deleteOne);
+app.put("/api/wilder", wilderController.updateOne);
 
 
 const start = async () => {
     await dataSource.initialize();
-    // dataSource.getRepository(Wilder).save({ name: "First Wilder" });
     app.listen(3000, () => console.log("Server started on 3000"));
 };
 //Start Server
